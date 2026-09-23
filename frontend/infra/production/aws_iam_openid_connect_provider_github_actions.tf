8064aede1bcc5889ebbaae40c2aaee0744540d5e
João Pedro <joaocarvalho.bahia@outlook.com>
2026-09-22 21:42:57 -0300

    deploy infra project

--- frontend/infra/production/aws_iam_openid_connect_provider_github_actions.tf
+++ frontend/infra/production/aws_iam_openid_connect_provider_github_actions.tf
@@ -36,12 +36,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:joaopedrogoncalvescarvalho/financy:environment:production",
-              "repo:joaopedrogoncalvescarvalho/financy:ref:refs/heads/main",
-              "repo:joaopedrogoncalvescarvalho/financy:ref:refs/heads/production",
-              "repo:joaopedrogoncalvescarvalho/financy:ref:refs/tags/*"
-            ]
+            "token.actions.githubusercontent.com:sub" = "repo:joaopedrogoncalvescarvalho/financy:*"
           }
         }
       }

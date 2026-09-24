b30718e84b7c917d912b5f536e0f9e5b1c151aac
João Pedro <joaocarvalho.bahia@outlook.com>
2026-09-23 22:03:54 -0300

    deploy infra project

--- frontend/infra/production/aws_iam_openid_connect_provider_github_actions.tf
+++ frontend/infra/production/aws_iam_openid_connect_provider_github_actions.tf
@@ -28,13 +28,17 @@ resource "aws_iam_role" "github_actions" {
     Statement = [
       {
         Effect = "Allow"
+
         Principal = {
           Federated = aws_iam_openid_connect_provider.github_actions.arn
         }
+
         Action = "sts:AssumeRoleWithWebIdentity"
+
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+
             "token.actions.githubusercontent.com:sub" = "repo:joaopedrogoncalvescarvalho@50954677/financy@1382482821:ref:refs/heads/main"
           }
         }
@@ -50,130 +54,8 @@ resource "aws_iam_role" "github_actions" {
   }
 }
 
-# IAM Policy for GitHub Actions
-resource "aws_iam_role_policy" "github_actions" {
-  name = "financy-github-actions-policy"
-  role = aws_iam_role.github_actions.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Sid    = "TerraformStateList"
-        Effect = "Allow"
-
-        Action = [
-          "s3:ListBucket"
-        ]
-
-        Resource = "arn:aws:s3:::infra-configure"
-
-        Condition = {
-          StringLike = {
-            "s3:prefix" = [
-              "financy/production/*"
-            ]
-          }
-        }
-      },
-
-      {
-        Sid    = "TerraformStateObjects"
-        Effect = "Allow"
-
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-
-        Resource = "arn:aws:s3:::infra-configure/financy/production/*"
-      },
-
-      {
-        Sid    = "FrontendBucket"
-        Effect = "Allow"
-
-        Action = [
-          "s3:ListBucket",
-          "s3:GetBucketAcl",
-          "s3:GetBucketPolicy",
-          "s3:GetBucketLocation",
-          "s3:GetBucketVersioning",
-          "s3:GetBucketWebsite",
-          "s3:GetBucketCors",
-          "s3:GetBucketLogging",
-          "s3:GetBucketTagging",
-          "s3:GetEncryptionConfiguration",
-          "s3:GetLifecycleConfiguration",
-          "s3:GetReplicationConfiguration",
-          "s3:GetBucketPublicAccessBlock",
-          "s3:GetBucketOwnershipControls",
-          "s3:GetBucketNotification",
-          "s3:GetAccelerateConfiguration",
-          "s3:GetBucketAccelerateConfiguration"
-        ]
-
-        Resource = aws_s3_bucket.frontend_static.arn
-      },
-
-      {
-        Sid    = "FrontendBucketObjects"
-        Effect = "Allow"
-
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectAcl",
-          "s3:PutObject",
-          "s3:PutObjectAcl",
-          "s3:DeleteObject",
-          "s3:ListBucketMultipartUploads",
-          "s3:ListMultipartUploadParts",
-          "s3:AbortMultipartUpload"
-        ]
-
-        Resource = "${aws_s3_bucket.frontend_static.arn}/*"
-      },
-
-      {
-        Sid    = "GithubActionsRole"
-        Effect = "Allow"
-
-        Action = [
-          "iam:GetRole",
-          "iam:CreateRole",
-          "iam:DeleteRole",
-          "iam:UpdateRole",
-          "iam:UpdateAssumeRolePolicy",
-          "iam:GetRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:DeleteRolePolicy",
-          "iam:ListRolePolicies",
-          "iam:ListAttachedRolePolicies",
-          "iam:ListInstanceProfilesForRole",
-          "iam:TagRole",
-          "iam:UntagRole"
-        ]
-
-        Resource = aws_iam_role.github_actions.arn
-      },
-
-      {
-        Sid    = "GithubOidcProvider"
-        Effect = "Allow"
-
-        Action = [
-          "iam:GetOpenIDConnectProvider",
-          "iam:CreateOpenIDConnectProvider",
-          "iam:UpdateOpenIDConnectProvider",
-          "iam:DeleteOpenIDConnectProvider",
-          "iam:TagOpenIDConnectProvider",
-          "iam:UntagOpenIDConnectProvider"
-        ]
-
-        Resource = aws_iam_openid_connect_provider.github_actions.arn
-      }
-    ]
-  })
+# Administrator access for Terraform
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
